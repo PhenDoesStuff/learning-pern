cdcdfe5d6b21071e784127731ce8050a94208483
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-09-18 22:37:30 -0400

    Created routes for getting all todos and creating a todo via REST api.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const pool = require('./db');
 
 // middleware to make sure we can get past CORS
 app.use((req, res, next) => {
@@ -11,5 +13,39 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	next();
 });
+app.use(bodyParser.json());
 
-app.listen(5000)
+// ROUTES
+
+// Create a Todo
+app.post('/todos', async (req, res, next) => {
+	let newTodo;
+	try {
+		const { description } = req.body;
+		newTodo = await pool.query(
+			'INSERT INTO todo (description) VALUES($1)',
+			[description]
+		);
+	} catch (err) {
+		return next(err);
+	}
+
+	res.status(201).json(newTodo);
+});
+
+// Get All Todos
+app.get('/todos', async (req, res, next) => {
+	let allTodos;
+	try {
+		allTodos = await pool.query('SELECT * FROM todo;');
+	} catch (error) {
+		return next(error);
+	}
+	res.status(200).json(allTodos.rows);
+});
+
+// Get a Todo
+
+// Update a Todo
+
+app.listen(5000);
